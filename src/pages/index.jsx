07e4ae071ee7ac34b13c798fd61ad00032b5f90e
vidhya0617 <vidhya.b@babychakra.com>
2022-04-29 16:15:27 +0530

    removed unwanted code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-import {test, add, Button} from 'node_modules/next-lib2/dist/index'
+import {Button} from 'node_modules/next-lib2/dist/index'
 
 const Home = () => {
-  console.log("vidhya test "+test(1,2))
-  console.log("vidhya add "+add())
 
   return (
     <>
