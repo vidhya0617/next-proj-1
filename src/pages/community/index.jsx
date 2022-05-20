056445c5feddfef1724f2ae79ba0cd1838206a81
vidhya0617 <vidhya.b@babychakra.com>
2022-05-20 17:18:45 +0530

    community page -- grid wrapper

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -4,13 +4,9 @@ import { FeedSection } from "next-lib-g3";
 
 const Community = () => {
   return (
-    <div>
-      <div className="md:p-20">
-        <div className="grid gap-5 items-start home-page-wrapper">
-          <div>
-            <FeedSection wrapperClassName="mb-3" />
-          </div>
-        </div>
+    <div className="md:p-20 ">
+      <div className="grid" style={{ gridTemplateColumns: "50%" }}>
+        <FeedSection wrapperClassName="mb-3" />
       </div>
     </div>
   );
