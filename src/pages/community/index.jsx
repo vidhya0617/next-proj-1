161825dd8e9e04ec6ec2cbcbe790665e92a32098
vidhya0617 <vidhya.b@babychakra.com>
2022-05-23 11:47:06 +0530

    installed new packages

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import { FeedSection } from "next-lib-g3";
 
 const Community = () => {
-  return (
-    <div className="md:p-20 ">
-      <div className="grid" style={{ gridTemplateColumns: "50%" }}>
-        <FeedSection wrapperClassName="mb-3" />
-      </div>
-    </div>
-  );
+  return <FeedSection />;
 };
 
 export default Community;
+
+// <div className="md:p-20 ">
+//   <div className="grid" style={{ gridTemplateColumns: "50%" }}>
+//   </div>
+// </div>
+
+// wrapperClassName = "mb-3";
